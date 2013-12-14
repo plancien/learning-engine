7b2486ae5b84dd72b6fbd71571e616f04e0f377e
Hach7ko <khemri.samya.isart@gmail.com>
2013-12-14 20:37:50 +0100

    Window Size
    Appel de la fonction à chaque resize de la fenêtre, à mettre en place en
    canvas pour créer un canvas qui s'adapte à la taille de la fenêtre.

--- public/scripts/games/testWindow.js
+++ public/scripts/games/testWindow.js
@@ -3,5 +3,8 @@ define([
     'modules/window_size'
 ], function (eventBus, window_size) {
 
-      eventBus.emit('get');
+    eventBus.emit('get');
+    window.onresize = function () {
+        eventBus.emit('get');
+    };
 });
--- public/scripts/modules/window_size.js
+++ public/scripts/modules/window_size.js
@@ -3,9 +3,9 @@ define(['event_bus'], function (eventBus) {
     eventBus.on('get', function() {
         var width = window.innerWidth;
         var height = window.innerHeight;
-        console.log('width: '+width+' height: '+height);
-        eventBus.emit('width', width);
-        eventBus.emit('height', height);
+
+        eventBus.on(width);
+        eventBus.on(height);
     });
 
 });
