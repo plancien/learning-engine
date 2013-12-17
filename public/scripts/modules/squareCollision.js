9ee6ec75315d6d01c9804a69ed4b2a8604458b6f
stara.alexis <stara.alexis.isart@gmail.com>
2013-12-17 17:15:11 +0100

    modify the square collision for proper working

--- public/scripts/modules/squareCollision.js
+++ public/scripts/modules/squareCollision.js
@@ -1,6 +1,5 @@
 define(['event_bus'], function (eventBus) {
 
-
     /***********************************************************************
       You Must Use PlugIn addColliderCapabilities in order to make it Works
       - Add all your objects collidable first
@@ -9,26 +8,29 @@ define(['event_bus'], function (eventBus) {
     
     function addSquareColliderCapabilities (object, collidableObjects)
     {
-        for (var i = 0; i < collidableObjects.length; i++) 
+        object.prototype.detectCollision = function ()
         {
-            if (collidableObjects[i] != object) 
+            for (var i = 0; i < collidableObjects.length; i++) 
             {
-                var colliderOfObject = collidableObjects[i].sides;
-                if ((object.y + object.height >= colliderOfObject[2].y) && (object.y <= colliderOfObject[2].y + colliderOfObject[2].height) && (object.x + object.width >= colliderOfObject[2].x) &&(object.x <= colliderOfObject[2].x + colliderOfObject[2].width))
+                if (collidableObjects[i] != object) 
                 {
-                    object.x += object.speed;
-                }
-                if ((object.y + object.height >= colliderOfObject[0].y) && (object.y <= colliderOfObject[0].y + colliderOfObject[0].height) && (object.x + object.width >= colliderOfObject[0].x) &&(object.x <= colliderOfObject[0].x + colliderOfObject[0].width))
-                {
-                    object.x -= object.speed;
-                }
-                if ((object.y + object.height >= colliderOfObject[1].y) && (object.y <= colliderOfObject[1].y + colliderOfObject[1].height) && (object.x + object.width >= colliderOfObject[1].x) &&(object.x <= colliderOfObject[1].x + colliderOfObject[1].width))
-                {
-                    object.y -= object.speed;
-                }
-                if ((object.y + object.height >= colliderOfObject[3].y) && (object.y <= colliderOfObject[3].y + colliderOfObject[3].height) && (object.x + object.width >= colliderOfObject[3].x) &&(object.x <= colliderOfObject[3].x + colliderOfObject[3].width))
-                {
-                    object.y += object.speed;
+                    var colliderOfObject = collidableObjects[i].sides;
+                    if ((object.y + object.height >= colliderOfObject[2].y) && (object.y <= colliderOfObject[2].y + colliderOfObject[2].height) && (object.x + object.width >= colliderOfObject[2].x) &&(object.x <= colliderOfObject[2].x + colliderOfObject[2].width))
+                    {
+                        object.x += object.speed;
+                    }
+                    if ((object.y + object.height >= colliderOfObject[0].y) && (object.y <= colliderOfObject[0].y + colliderOfObject[0].height) && (object.x + object.width >= colliderOfObject[0].x) &&(object.x <= colliderOfObject[0].x + colliderOfObject[0].width))
+                    {
+                        object.x -= object.speed;
+                    }
+                    if ((object.y + object.height >= colliderOfObject[1].y) && (object.y <= colliderOfObject[1].y + colliderOfObject[1].height) && (object.x + object.width >= colliderOfObject[1].x) &&(object.x <= colliderOfObject[1].x + colliderOfObject[1].width))
+                    {
+                        object.y -= object.speed;
+                    }
+                    if ((object.y + object.height >= colliderOfObject[3].y) && (object.y <= colliderOfObject[3].y + colliderOfObject[3].height) && (object.x + object.width >= colliderOfObject[3].x) &&(object.x <= colliderOfObject[3].x + colliderOfObject[3].width))
+                    {
+                        object.y += object.speed;
+                    }
                 }
             }
         }
