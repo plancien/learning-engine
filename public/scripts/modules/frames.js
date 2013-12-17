1a888ebc86ff530ccf059e57d73b21585558941e
U-benjamin-ISART\benjamin <benjamin@benjamin-ISART.(none)>
2013-12-17 12:50:53 +0100

    replace tabs by spaces inside all js files

--- public/scripts/modules/frames.js
+++ public/scripts/modules/frames.js
@@ -2,12 +2,12 @@ define(['event_bus'], function (eventBus) {
 
     var requestAnimationFrame = window.requestAnimationFrame
         || window.webkitRequestAnimationFrame
-    	|| window.mozRequestAnimationFrame
-    	|| window.oRequestAnimationFrame
-    	|| window.msRequestAnimationFrame
-    	|| function(callback) {
-    	    window.setTimeout(callback, 1000 / 60);
-    	};
+        || window.mozRequestAnimationFrame
+        || window.oRequestAnimationFrame
+        || window.msRequestAnimationFrame
+        || function(callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
     
     var onEachFrame = function() {
         eventBus.emit('new frame');
--- public/scripts/modules/player.js
+++ public/scripts/modules/player.js
@@ -1,18 +1,18 @@
 define(['event_bus'], function (eventBus) {
 
-	function Player(x, y, width, height, life, hitboxDistance, damageDeal, speed)
-	{
-		this.x = x;
-		this.y = y;
-		this.life = life;
-		this.width = width;
-		this.height	= height;
-		this.hitboxDistance = hitboxDistance;
-		this.damageDeal = damageDeal;
-		this.speed = speed;
-	}
+    function Player(x, y, width, height, life, hitboxDistance, damageDeal, speed)
+    {
+        this.x = x;
+        this.y = y;
+        this.life = life;
+        this.width = width;
+        this.height = height;
+        this.hitboxDistance = hitboxDistance;
+        this.damageDeal = damageDeal;
+        this.speed = speed;
+    }
 
-	eventBus.on('init player', function (object, x, y, width, height, life, hitboxDistance, damageDeal, speed) {
-		object.push(new Player(x, y, width, height, life, hitboxDistance, damageDeal, speed));
+    eventBus.on('init player', function (object, x, y, width, height, life, hitboxDistance, damageDeal, speed) {
+        object.push(new Player(x, y, width, height, life, hitboxDistance, damageDeal, speed));
     });
 });
