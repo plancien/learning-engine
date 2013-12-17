f5f9bd6350868ed6b1024fe609db1fa5699547e0
stara.alexis <stara.alexis.isart@gmail.com>
2013-12-17 17:22:31 +0100

    change squareCollision.js for just detect the collision and let the dev code the reaction init by the collision. the collision detection detect if the object enter with the other by left/right/top/bottom.

--- public/scripts/modules/squareCollision.js
+++ public/scripts/modules/squareCollision.js
@@ -17,19 +17,19 @@ define(['event_bus'], function (eventBus) {
                     var colliderOfObject = collidableObjects[i].sides;
                     if ((object.y + object.height >= colliderOfObject[2].y) && (object.y <= colliderOfObject[2].y + colliderOfObject[2].height) && (object.x + object.width >= colliderOfObject[2].x) &&(object.x <= colliderOfObject[2].x + colliderOfObject[2].width))
                     {
-                        object.x += object.speed;
+                        eventBus.emit('collision by right', object, collidableObjects);
                     }
                     if ((object.y + object.height >= colliderOfObject[0].y) && (object.y <= colliderOfObject[0].y + colliderOfObject[0].height) && (object.x + object.width >= colliderOfObject[0].x) &&(object.x <= colliderOfObject[0].x + colliderOfObject[0].width))
                     {
-                        object.x -= object.speed;
+                        eventBus.emit('collision by left', object, collidableObjects);
                     }
                     if ((object.y + object.height >= colliderOfObject[1].y) && (object.y <= colliderOfObject[1].y + colliderOfObject[1].height) && (object.x + object.width >= colliderOfObject[1].x) &&(object.x <= colliderOfObject[1].x + colliderOfObject[1].width))
                     {
-                        object.y -= object.speed;
+                        eventBus.emit('collision by top', object, collidableObjects);
                     }
                     if ((object.y + object.height >= colliderOfObject[3].y) && (object.y <= colliderOfObject[3].y + colliderOfObject[3].height) && (object.x + object.width >= colliderOfObject[3].x) &&(object.x <= colliderOfObject[3].x + colliderOfObject[3].width))
                     {
-                        object.y += object.speed;
+                        eventBus.emit('collision by bottom', object, collidableObjects);
                     }
                 }
             }
