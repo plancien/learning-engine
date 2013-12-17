ee4829d99047cea80db5abd8948c399369f6d9c4
ashleymoogle <romain.bonhomme89@gmail.com>
2013-12-17 15:50:51 +0100

    Cleaning my shit

--- public/scripts/modules/player.js
+++ public/scripts/modules/player.js
@@ -2,10 +2,8 @@ define(['event_bus'], function (eventBus) {
 
     function Player(properties)
     {
-        //All the properties of the player that will be pushed at its creation
         for (var key in properties) {
-            this[key] = properties[key];
-            finalProperties.push(properties[key]);
+            this[key] = properties[key]; //ie this.x = x;
         }
     }
 
