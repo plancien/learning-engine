094d4b8e77effa487c48531e3d28a89d02cd6fe4
U-benjamin-ISART\benjamin <benjamin@benjamin-ISART.(none)>
2013-12-17 12:35:05 +0100

    replace tab by space OLO into chrono.js

--- public/scripts/modules/chrono.js
+++ public/scripts/modules/chrono.js
@@ -2,47 +2,47 @@ define(['event_bus'], function (eventBus) {
 
     var stopWatchStart = false;
     var timeStamp = null;
-	var giveTime = 
-	{
-		fullTime : 0,
-		hours : 0,
-		minutes : 0,
-		seconds : 0
-	};
+    var giveTime = 
+    {
+        fullTime : 0,
+        hours : 0,
+        minutes : 0,
+        seconds : 0
+    };
 
-	eventBus.on('start stopwatch', function()
-	{
-		timeStamp = new Date().getTime();
-		stopWatchStart = true;
-	});
+    eventBus.on('start stopwatch', function()
+    {
+        timeStamp = new Date().getTime();
+        stopWatchStart = true;
+    });
 
-	eventBus.on('stop stopwatch', function()
-	{
-		timeStamp = null;
-		stopWatchStart = false;
-	});
+    eventBus.on('stop stopwatch', function()
+    {
+        timeStamp = null;
+        stopWatchStart = false;
+    });
 
-	eventBus.on('new frame', function()
-	{
-		if(stopWatchStart)
-		{
-			var compareStamp = new Date().getTime();
-			giveTime.seconds = Math.round((compareStamp - timeStamp) / 1000);
+    eventBus.on('new frame', function()
+    {
+        if(stopWatchStart)
+        {
+            var compareStamp = new Date().getTime();
+            giveTime.seconds = Math.round((compareStamp - timeStamp) / 1000);
 
-			if(giveTime.seconds > 60){
-				giveTime.minutes++;
-				timeStamp = new Date().getTime();
-			}
+            if(giveTime.seconds > 60){
+                giveTime.minutes++;
+                timeStamp = new Date().getTime();
+            }
 
-			if(giveTime.minutes > 60){
-				giveTime.hours++;
-				giveTime.minutes = 0;
-			}
+            if(giveTime.minutes > 60){
+                giveTime.hours++;
+                giveTime.minutes = 0;
+            }
 
-			giveTime.fullTime = giveTime.hours + " : " + giveTime.minutes + " : " + giveTime.seconds;
-			eventBus.emit('get stopwatch', giveTime);
-		}
-	});
+            giveTime.fullTime = giveTime.hours + " : " + giveTime.minutes + " : " + giveTime.seconds;
+            eventBus.emit('get stopwatch', giveTime);
+        }
+    });
 
 });
 
