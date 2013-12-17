d00f7e2325aa1c718fdf283d7a52da885d2fb284
ashleymoogle <romain.bonhomme89@gmail.com>
2013-12-17 17:35:10 +0100

    /!\ NEW /!\ NOW WITH EVENTS (and coms.) !
    events for death, hp loss, damage done and moar coms.

--- public/scripts/modules/player.js
+++ public/scripts/modules/player.js
@@ -1,13 +1,31 @@
-define(['event_bus'], function (eventBus) {
+define(['event_bus','modules/playerTab'], function (eventBus) {
 
-    function Player(properties)
+    function Player(properties) //where properties is an associative array ie : new Player({speed : "45", life : "10", spritesheet : "skin.png"})
     {
         for (var key in properties) {
             this[key] = properties[key]; //ie this.x = x;
         }
-    }
+    
+    // ****************** EMITERS ****************************
+        function doDamage(id){
+            eventBus.emit('damage' + id, this.damage);
+        }
 
-    eventBus.on('init player', function (properties) {
-        playerTab.push(new Player(properties));
-    });
+    // ****************** RECEIVERS ****************************
+
+        eventBus.on('init player', function (properties) {
+            playerTab.push(new Player(properties));
+        });
+
+        eventBus.on('damage' + this.id, function(damage) {
+            this.hp -= damage;
+            if (this.hp<= 0){
+                eventBus.emit('player dead' + this.id);
+            }
+        }, this);
+
+        eventBus.on('player dead' + this.id, function() {
+            playerTab.splice(playerTab.indexOf(this["id"]),1);
+        }, this);
+    }
 });
