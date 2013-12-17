7eda599f4a0aeadf051c1052680584e711b5e238
Ludovic <ludovic.leng@gmail.com>
2013-12-17 14:40:51 +0100

    Patch scoring

--- public/scripts/modules/scoring.js
+++ public/scripts/modules/scoring.js
@@ -5,16 +5,16 @@ define(['event_bus'], function (eventBus) {
         multi : 1,
     }
     
-    eventBus.on("add score", function(param) {
-        score.value += param.score  * score.multi;
+    eventBus.on("add score", function(score) {
+        score.value += score  * score.multi;
     });
     
-    eventBus.on("lost score", function(param) {
-        score.value -= param.score;
+    eventBus.on("lost score", function(score) {
+        score.value -= score;
     });
         
-    eventBus.on("multiplicator", function(param) {
-        score.multi = param.multi;
+    eventBus.on("multiplicator", function(multi) {
+        score.multi = multi;
     });
     
     return score;
