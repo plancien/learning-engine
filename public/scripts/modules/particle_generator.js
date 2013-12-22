4d6103127fa250ea873c6dd53834070c536ee631
TagliatL <maurer.paul@ymail.com>
2013-12-22 16:04:26 +0100

    Particle module Finale Version wow
    More features and improvements. It works !

--- public/scripts/modules/particle_generator.js
+++ public/scripts/modules/particle_generator.js
@@ -1,48 +1,55 @@
 define(['event_bus','modules/frames'], function(eventBus){
 
-	var particleTable = [];
-	var context = null;
+    var particleTable = [];
+    var context = null;
+    eventBus.on('newCanvas', function(canvas){
+        context = canvas.context;
+    });
 
-	eventBus.on('newCanvas', function(canvas){
-		context = canvas.context;
-	});
+    var Particle = function(x, y, color, duration) {
+        this.x = x;
+        this.y = y;
+        var heightWidth = 5;
+        var fix=5;
+        var change;
+        this.duration = duration;
+        this.speed = 4;
+        this.angle = Math.random() *Math.PI *2;
+        this.color = color;
+        this.move = function() {
+            //mouvements aleatoirs en angle
+            this.x += Math.cos(this.angle)* this.speed;
+            this.y -= Math.sin(this.angle)* this.speed;
+            if(this.duration <= 0)
+            {
+                particleTable.splice(this, 1);
+            }
+        }
+        this.draw = function() {    
+            context.fillStyle = this.color;
+            context.fillRect(this.x, this.y, heightWidth, heightWidth );
+        }
+    }
 
-	var Particle = function(x, y, color, canvasWidth) {
-		this.x = x;
-		this.y = y;
-		var disapear = 0;
-		var fix=5;
-		var change=disapear;
-		this.speed = 4;
-		this.angle = Math.random() *Math.PI *2;
-		this.color = color;
+    eventBus.on('new frame', function(){
+        if(particleTable[0] != undefined)
+        {
+            context.clearRect(0,0,1200,1200);
+        }
+        for (var i = 0; i < particleTable.length ; i++)
+        {
+            particleTable[i].move();
+            particleTable[i].draw();
+            particleTable[i].duration--;
 
-		this.move = function() {
-			//mouvements aleatoirs en angle
-			this.x += Math.cos(this.angle)* this.speed;
-			this.y -= Math.sin(this.angle)* this.speed;
-			disapear =-1;
-			change=disapear;
-		}
-		this.draw = function() {	
-			context.fillStyle = this.color;
-			context.fillRect(this.x, this.y, 5 + change , 5 + change);
-		}
-	}
+        }
+    });
 
-	eventBus.on('new frame', function(){
-		for (var i = 0; i < particleTable.length ; i++)
-	    {
-	        particleTable[i].move();
-	        particleTable[i].draw();
-	    }
-	});
-
-	eventBus.on('CreateParticles', function(x, y, color, count){
-		for (var i = 0; i < count; i++)
-		{
-			var star = new Particle(x, y, color);
-			particleTable.push(star);
-		}
-	});
+    eventBus.on('CreateParticles', function(x, y, color, count, duration){
+        for (var i = 0; i < count; i++)
+        {
+            var star = new Particle(x, y, color, duration);
+            particleTable.push(star);
+        }
+    });
 });
