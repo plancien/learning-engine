f4d1199789ceb67eaf1943802a181a72d412ed4a
Hach7ko <khemri.samya.isart@gmail.com>
2013-12-31 14:51:51 +0100

    Modif Snake Game
    Add the snake game without comms and one major bug

--- public/scripts/games/snake.js
+++ public/scripts/games/snake.js
@@ -26,19 +26,125 @@ define([
     }
 
     var canvas = Canvas.create(params);
-    var context = canvas.context;
+    var ctx = canvas.context;
+
+    var h = taille.height;
+    var w = taille.width
+
+    var cw = 10;
+    var d;
+    var food;
+    var score;
     
-    context.fillStyle = "#000000";
-    context.fillRect(0, 0, taille.width-200, taille.height-200);
+    var snake_array;
+    
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, taille.width-200, taille.height-200);
     
     window.onresize = function () {
-        context.clearRect(0, 0, taille.width, taille.height)
+        ctx.clearRect(0, 0, w, h);
         taille = WindowSize.getWindowSize();
-        context.fillStyle = "#000000";
-        context.fillRect(0, 0, taille.width-200, taille.height-200);
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(0, 0, taille.width-200, taille.height-200);
+        w = taille.width - 200;
+        h = taille.height - 200;
     };
 
-     function gameLoop () {
-        setTimeout(gameLoop, 200 + Math.random()*300);
+    
+    function init() {
+        d = "right";
+        create_snake();
+        create_food();
+
+        score = 0;
+        
+        if(typeof game_loop != "undefined") clearInterval(game_loop);
+        game_loop = setInterval(paint, 60);
     }
+
+    init();
+    
+    function create_snake() {
+        var length = 5; 
+        snake_array = [];
+        for(var i = length-1; i>=0; i--) {
+            snake_array.push({x: i, y:0});
+        }
+    }
+    
+    function create_food() {
+        food = {
+            x: Math.round(Math.random()*(w-cw)/cw), 
+            y: Math.round(Math.random()*(h-cw)/cw), 
+        };
+    }
+    
+    function paint() {
+        ctx.clearRect(0, 0, w, h);
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(0, 0, w, h);
+
+        var nx = snake_array[0].x;
+        var ny = snake_array[0].y;
+
+        if(d == "right") nx++;
+        else if(d == "left") nx--;
+        else if(d == "up") ny--;
+        else if(d == "down") ny++;
+
+        if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array)) {
+            init();
+            return;
+        }
+
+        if(nx == food.x && ny == food.y) {
+            var tail = {x: nx, y: ny};
+            score++;
+
+            create_food();
+        }
+        else {
+            var tail = snake_array.pop();
+            tail.x = nx; tail.y = ny;
+        }
+        
+        snake_array.unshift(tail);
+        
+        for(var i = 0; i < snake_array.length; i++) {
+            var c = snake_array[i];
+
+            paint_cell(c.x, c.y);
+        }
+        
+
+        paint_cell(food.x, food.y);
+
+        var score_text = "Score: " + score;
+        ctx.fillText(score_text, 5, h-5);
+    }
+
+    function paint_cell(x, y) {
+        ctx.fillStyle = "green";
+        ctx.fillRect(x*cw, y*cw, cw, cw);
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(x*cw, y*cw, cw, cw);
+    }
+    
+    function check_collision(x, y, array) {
+
+        for(var i = 0; i < array.length; i++) {
+            if(array[i].x == x && array[i].y == y)
+             return true;
+        }
+        return false;
+    }
+
+    $(document).keydown(function(e) {
+        var key = e.which;
+
+        if(key == "37" && d != "right") d = "left";
+        else if(key == "38" && d != "down") d = "up";
+        else if(key == "39" && d != "left") d = "right";
+        else if(key == "40" && d != "up") d = "down";
+    })    
 });
