c80eb2f959028135feb3b66f63713ba4ce5d7e7a
FONTAINE THOMAS <tom_1993@hotmail.fr>
2014-01-08 11:52:33 +0100

    Adding images with the render module

--- public/scripts/game_models/learningShooter.js
+++ public/scripts/game_models/learningShooter.js
@@ -17,13 +17,19 @@ define([
     'modules/score',
     'modules/frames',
     'modules/mouse',
-], function (eventBus, canvasCreate, windowSize, score, frames, mouse) {
+    'modules/key_listener',
+    'modules/render',
+    'modules/bonus_chooser',
+    'modules/bonus_fader'
+], function (eventBus, canvasCreate, windowSize, score, frames, mouse,keyListener, Render) {
 
     return function(params)
     {
     	var canvas,
     	ctx = "";
-
+    	var img = new Image();
+    	img.src = "./images/GDP2.png";
+    	
 
     	var paramsCanvas = {
 				id: "learningShooter",
@@ -32,38 +38,54 @@ define([
 			};
 
     	var gameContainer = {
-    		paddle : ""
-    	}
+    		paddle : "",
+    		key : "",
+    		arrayArrows : [],
+    		ilBouge : {},
+    		imageGood : new Image(),
+    		imageBad :  new Image()
+    	};
+
+    	var images2 = {
+
+    	};
 
 	    eventBus.on('init', function () {
-
 			canvas = canvasCreate.create(paramsCanvas);
-			ctx = canvas.contxt;
+			ctx = canvas.context;
 
-		    ctx.fillRect(0, 0, paramsCanvas.width, paramsCanvas.height);
-		    ctx.fillStyle = "#000000";
+		   ctx.fillStyle = "black";
+		   ctx.fillRect(0, 0, paramsCanvas.width, paramsCanvas.height);
 
 		    var paramsPaddle = {
-		    	x : 10, 
-		    	y : 10,
-		    	width : 200,
-		    	height : 200
+		    	x : 350, 
+		    	y : 780,
+		    	width : 100,
+		    	height : 20,
+		    	speed : 5,
+		    	color : "rgb(255,255,255)"
 		    }
 
-		    gameContainer.paddle = new Paddle(paramsPaddle);
-	  	});
-
-
-
-	  	eventBus.on("new frame", function(){
-
-			ctx.fillStyle = "#00FF00";
-	  		ctx.clearRect(0, 0, paramsCanvas.width, params.height);
-
-		   	//Rendering and moving the paddle
-		   	gameContainer.paddle.draw();
-		   	gameContainer.paddle.move();
+		    gameContainer.paddle = new Paddle(paramsPaddle);	  	
 		});
+
+
+		eventBus.on("new frame", function(){
+				eventBus.emit('need new bonus');
+		  		ctx.fillStyle = "black";
+		  		ctx.fillRect(0, 0, paramsCanvas.width, paramsCanvas.height);
+			   	//Rendering and moving the paddle
+			   	gameContainer.paddle.update();
+
+			   	for(var i =0 ; i < gameContainer.arrayArrows.length; i++)
+			   	{	
+			   		gameContainer.arrayArrows[i].update();
+			   		if(gameContainer.arrayArrows[i].y < 0)
+			   		{
+			   			gameContainer.arrayArrows.splice(i, 1);
+			   		}
+			   	}
+			});
 
 		eventBus.on('mouse update', function(mouseMove)
 		{
@@ -71,34 +93,131 @@ define([
 		});
 
 
-	    var Paddle = function(params)
+/***************************************************************************************
+* PADDLE
+***************************************************************************************/
+	    var Paddle = function Paddle(params)
 	    {
+	    	this.f = 0
 	    	this.x = params.x;
 	    	this.y = params.y;
 	    	this.height = params.height;
 	    	this.width = params.width;
+	    	this.speed = params.speed;
+	    	this.color = params.color
 
-	    	this.draw = function()
+	    	this.draw = function draw()
 	    	{
 	    		ctx.beginPath();
 			    ctx.rect(this.x, this.y, this.width, this.height);
-			    ctx.fillStyle = "#00FF00";
+			    ctx.fillStyle = this.color;
 			    ctx.fill();
 			    ctx.closePath();
 	    	}	
 
-	    	this.move = function()
+	    	this.inputs = function inputs()
 	    	{
-	    		this.x++;
-	    		this.y++;
+	    		this.f++;
+	    		for(var i =0; i < gameContainer.key.length; i++)
+	    		{
+	    			if(this.x > 0)
+	    			{
+		    			if(gameContainer.key[i] == 81){
+		    				this.moveLeft();
+		    			}
+		    		}
+
+    				if(this.x < 700)
+	    			{
+		    			if(gameContainer.key[i] == 68){
+		    				this.moveRight();
+		    			}
+		    		}
+
+		    		if(gameContainer.key[i] == 32 && this.f%5 === 0){
+		    			this.shoot();
+		    		}
+
+	    		}
 	    	}
 
-	    	this.update = function()
+	    	this.moveLeft = function moveLeft()
 	    	{
-	    		this.move();
+	    		this.x -= this.speed ;
+	    	}
+
+	    	this.moveRight = function moveRight()
+	    	{
+				this.x += this.speed;
+	    	}
+
+
+	    	this.shoot = function shoot()
+	    	{
+	    		params.x = this.x + (this.width/2);
+	    		params.y = this.y;
+	    		params.height = 80;
+	    		params.width = 80;
+	    		var arrow = new Arrow(params);
+	    		var spritex = Math.round(Math.random()*9) * 48;
+	    		var spritey = Math.round(Math.random()*3) * 62;
+	    		eventBus.emit('init render', {object : arrow,
+                                      		  sprite : {x : spritex, y : spritey, width : 48, height : 62, img : img},
+                                      		  rotating : true
+                                    		  } )
+	    		var arrows = gameContainer.arrayArrows.push(arrow);
+	    	}
+
+	    	this.update = function update()
+	    	{
+	    		this.inputs();
 	    		this.draw();
 	    	}
 	    }
+
+/***************************************************************************************
+* Arrow shooted from the paddle
+***************************************************************************************/
+
+	    var Arrow = function Arrow(params)
+	    {
+	    	this.x = params.x;
+	    	this.y = params.y;
+	    	this.rotation = Math.random()*4;
+	    	this.radius = params.height/2;
+	    	this.width = this.radius;
+	    	this.height = this.radius;
+	    	this.speed = params.speed;
+	    	this.color = params.color;
+	    	this.image = gameContainer.imageGood;
+
+	    	this.move = function inputs()
+	    	{
+	    		this.y -= this.speed;
+	    	}
+
+	    	this.update = function update()
+	    	{
+	    		this.rotation+= 0.05;
+	    		this.move();
+	    		eventBus.emit('render object', this, ctx);
+	    	}
+	    }
+
+		eventBus.on('add bonus', function (good, url) {
+			gameContainer.imageGood.src = "./images/+.png";
+		});
+
+	   	eventBus.on('keys still pressed', function(data)
+	   	{
+	   	 	gameContainer.key = data;
+	   	});
+
+        var bonusPoints = params.bonusPoints || 1;
+        var malusPoints = params.malusPoints || -3;
+
+	   	eventBus.emit('init bonus', false, params.bonusUrl);
+        eventBus.emit('init bonus', true,  params.malusUrl);
 
 	};
 });
