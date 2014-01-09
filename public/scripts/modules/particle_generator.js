6cbd536c3f1ff78b387833c252ad1a879e55b7f4
FONTAINE_THOMAS <thomas.fontaine.tf@gmail.com>
2014-01-10 00:48:31 +0100

    Pushing pre-finale version
    - Adding particles
    - Adding mouse colision with ennemies

--- public/scripts/modules/particle_generator.js
+++ public/scripts/modules/particle_generator.js
@@ -1,55 +1,61 @@
 define(['event_bus','modules/frames'], function(eventBus){
+    return function() {
 
-    var particleTable = [];
-    var context = null;
-    eventBus.on('newCanvas', function(canvas){
-        context = canvas.context;
-    });
+        var particleTable = [];
+        var context = "";
+        eventBus.on('newCanvas', function(canvas){
+            context = canvas.context;
+        });
 
-    var Particle = function(x, y, color, duration) {
-        this.x = x;
-        this.y = y;
-        var heightWidth = 5;
-        var fix=5;
-        var change;
-        this.duration = duration;
-        this.speed = 4;
-        this.angle = Math.random() *Math.PI *2;
-        this.color = color;
-        this.move = function() {
-            //mouvements aleatoirs en angle
-            this.x += Math.cos(this.angle)* this.speed;
-            this.y -= Math.sin(this.angle)* this.speed;
-            if(this.duration <= 0)
+        var Particle = function(x, y, color, duration) {
+            this.x = x;
+            this.y = y;
+            var heightWidth = 5;
+            var fix=5;
+            var change;
+            this.duration = duration;
+            this.speed = 4;
+            this.angle = Math.random() *Math.PI *2;
+            this.color = color;
+            this.move = function() {
+                //mouvements aleatoirs en angle
+                this.x += Math.cos(this.angle)* this.speed;
+                this.y -= Math.sin(this.angle)* this.speed;
+                if(this.duration <= 0)
+                {
+                    particleTable.splice(particleTable.indexOf(this),1);
+                }
+            }
+            this.draw = function() {
+                context.fillStyle = this.color;
+                context.fillRect(this.x, this.y, heightWidth, heightWidth);
+            }
+
+            this.update = function update()
             {
-                particleTable.splice(particleTable.indexOf(this),1);
+                this.duration--;
+                this.draw();
+                this.move();
             }
         }
-        this.draw = function() {    
-            context.fillStyle = this.color;
-            context.fillRect(this.x, this.y, heightWidth, heightWidth );
-        }
+
+       eventBus.on("new frame", function(){
+            for (var i = 0; i < particleTable.length ; i++)
+            {
+                if(particleTable[i] != undefined)
+                {
+                   particleTable[i].update();
+                }
+            }
+        });
+
+        eventBus.on('CreateParticles', function(x, y, color, count, duration){
+
+            for (var i = 0; i < count; i++)
+            {
+                var star = new Particle(x, y, color, duration);
+                particleTable.push(star);
+            }  
+        });
     }
-
-    eventBus.on('new frame', function(){
-        if(particleTable[0] != undefined)
-        {
-            context.clearRect(0,0,1200,1200);
-        }
-        for (var i = 0; i < particleTable.length ; i++)
-        {
-            particleTable[i].move();
-            particleTable[i].draw();
-            particleTable[i].duration--;
-
-        }
-    });
-
-    eventBus.on('CreateParticles', function(x, y, color, count, duration){
-        for (var i = 0; i < count; i++)
-        {
-            var star = new Particle(x, y, color, duration);
-            particleTable.push(star);
-        }
-    });
 });
