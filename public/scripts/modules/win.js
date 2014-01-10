61e2b77ec3a9e9f4b871292ca33fcd18d325b3d3
breizhpanda <kieffer41@hotmail.fr>
2014-01-10 18:22:17 +0100

    Add win module

---
+++ public/scripts/modules/win.js
@@ -0,0 +1,17 @@
+define(['event_bus',
+		'modules/window_size',
+		'modules/add_domElem',
+		'modules/canvas'], 
+		function (eventBus,WindowSize,Canvas) {
+	eventBus.on("gameover",function()
+	{
+		function replay()
+		{
+			location.reload();
+		}
+		var taille = WindowSize.getWindowSize();
+		var cssgameover = "width:"+taille.width+"px;height:"+taille.height+"px;position:absolute;cursor:pointer;z-index:15;background:#00CCFF url('../images/win.png') no-repeat center;";	
+		eventBus.emit("createElement", {elem:"div", stylesheet:csswin, id:"win", typeEvent:"click", event:replay});
+		
+	});
+});
