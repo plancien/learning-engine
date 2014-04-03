15101a878b72bab73c0d5debcd0b23552fd0c067
Pierre Lancien <pierre@toxicode.fr>
2014-04-04 00:36:47 +0200

    bugfixes

--- public/scripts/game_types/frogger.js
+++ public/scripts/game_types/frogger.js
@@ -20,6 +20,32 @@ define([
     'modules/score'
 ], function(eventBus, canvasModule, render, imageLoader, frames, keyListner, scoreModule) {
 
+    
+    
+
+    var isInside = function inInside (objectB){
+        if(this.x < objectB.x){
+            if(this.x + this.width/2 > objectB.x - objectB.width/2){
+                if(this.y < objectB.y){
+                    if(this.y + this.height/2 > objectB.y - objectB.height/2){
+                        return true;
+                    }
+                }else if(this.y - this.width/2 < objectB.y + objectB.width/2){
+                    return true;
+                }
+            }
+        }else if(this.x - this.width/2 < objectB.x + objectB.width/2){
+            if(this.y < objectB.y){
+                if(this.y + this.height/2 > objectB.y - objectB.height/2){
+                        return true;
+                    }
+            }else if(this.y - this.width/2 < objectB.y + objectB.width/2){
+                return true;
+            }
+        }
+        return false;
+    }
+
     return function(globalParams) {
 
         var paramsCanvas = {
