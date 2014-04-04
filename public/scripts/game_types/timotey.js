c5c0411aaaa81184e0c86c02938ccdf9d8412f98
Woreck <titim-du-60@hotmail.fr>
2014-04-04 12:59:22 +0200

    Add structure timotey.js
    Contient une base pour du canvas

--- public/scripts/game_types/timotey.js
+++ public/scripts/game_types/timotey.js
@@ -1,6 +1,41 @@
 define([
-	'event_bus',
-	'modules/canvas',
-	],function(){
+    'event_bus',
+    'modules/canvas',
+    'modules/frames',
+], function (eventBus,canvasCreate,framer,Game) {	//Déclare les variables contenant les modules chargé dans define([])
 
+	//Le module retourné
+    return function(params) {
+        var container;
+        //équivalent du window.onload
+        eventBus.on('init', function (_container) {
+        	//???
+             container = _container;
+             //Ici je récupère le canvas, canvasCreate est le module canvas.js chargé plus haut
+             var canvas = canvasCreate.create({width:800,height:600,});
+             var context = canvas.context;
+             //Je créé un carré que l'on affichera
+             var box = {
+             	x:0,
+             	y:0,
+             	w:100,
+             	h:100,
+             	color: 'red'
+             };
+
+             context.fillStyle = "black";
+             context.fillRect(0,0,canvas.canvas.width,canvas.canvas.height);
+             //On ajoute un écouteur sur 'new frame', évènement défini dans frames.js
+             //Le callback correspond à notre mainLoop / Update 
+             eventBus.on('new frame', function(){
+             	context.fillStyle = "black";
+             	context.fillRect(0,0,canvas.canvas.width,canvas.canvas.height);
+             	
+             	box.x++;
+
+             	context.fillStyle = box.color;
+             	context.fillRect(box.x,box.y,box.w,box.h);
+             });
+        });
+    }
 });
