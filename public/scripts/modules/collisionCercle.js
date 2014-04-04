1ae49c2b7de36ceeac156891a8c9eb295e3d535d
RosiniUgo <rosini.u@gmail.com>
2014-04-04 18:01:36 +0100

    Modification collisionCercle

--- public/scripts/modules/collisionCercle.js
+++ public/scripts/modules/collisionCercle.js
@@ -1,8 +1,8 @@
 define(['event_bus'], function(eventBus) {
 
-	eventBus.on('collisionCercle', function(objet1,objet2,rayonObjet1,rayonObjet2) 
+	eventBus.on('collisionCercle', function(object1,object2,radiusObject1,radiusObject2) 
 	{
-	if(Math.abs(objet1.x-objet2.x)+Math.abs(objet1.y-objet2.y)<= rayonObjet1+rayonObjet2)
+	if(Math.abs(object1.x-object2.x)+Math.abs(object1.y-object2.y)<= rayonObject1+radiusObject2)
 		return true;
 	else
 		return false;
