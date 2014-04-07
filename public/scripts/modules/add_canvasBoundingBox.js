cde38e56ee17fac770aab9fc69bf4500857e25c2
Woreck <titim-du-60@hotmail.fr>
2014-04-08 00:19:16 +0200

    canvasBoundingBox -> new params
    Return now EntirelyOutOn and PartiallyOutOn in function of the target
    send

--- public/scripts/modules/add_canvasBoundingBox.js
+++ public/scripts/modules/add_canvasBoundingBox.js
@@ -2,43 +2,66 @@ define(['event_bus'], function(eventBus) {
     
 
     /**********************************************
+
     	Add eventBus.emit('outside canvas',{canvas:myCanvas,target:myTarget}); to use it.
     		WATCHOUT /!\
     			myTarget is an object as this {x:NUMBER,y:NUMBER,w:NUMBER,h:NUMBER}; 
     		SUCCESS NOT GUARANTEED IF NOT STRUCTURED LIKE THIS
+
     	Will send an object 
     		object.isOutOnX = true/false,
-    		object.isOutOnY = true/false 
+    		object.isOutOnY = true/false
+
     	To catch the response add,
     	eventBus.on('outside canvas response',function(params){
-			if(params.isOutOnX){
-	
+			if(params.isEntirelyOutOnX){
+				//SPRITE ENTIRELY OUT OF THE CANVAS SCREEN ON THE X AXIS
 			}
-			if(params.isOutOnY){
-	
+			if(params.isPartiallyOutOnX){
+				//SPRITE IS PARTIALLY OUT OF THE CANVAS SCREEN ON THE X AXIS
 			}
+			if(params.isEntirelyOutOnY){
+				//SPRITE IS ENTIRELY OUT OF THE CANVAS SCREEN ON THE Y AXIS
+			}
+			if(params.isPartiallyOutOnY){
+				//SPRITE IS PARTIALLY OUT OF THE CANVAS SCREEN ON THE Y AXIS
+			}
+			
     	});
     **********************************************/
     eventBus.on('outside canvas', function(params) {
     	var canvas = params.canvas;
     	var target = params.target;
     	var paramsToSend = {
-    		isOutOnX:false,
-    		isOutOnY:false
+    		isEntirelyOutOnX:false,
+    		isPartiallyOutOnX:false,
+    		isEntirelyOutOnY:false,
+    		isPartiallyOutOnY:false
     	};
+    	//Try to change if forgot to make it w/h
     	if(!target.w){
     		target.w = target.width || 0;
     	}
     	if(!target.h){
     		target.h = target.height || 0;
     	}
+    	//outside X
+    	if(target.x+target.w < 0 ||
+    		target.x > canvas.width){
+    		params.isEntirelyOutOnX = true;
+    	}
     	if(target.x < 0 ||
     		target.x+target.w > canvas.width){
-    		params.isOutOnX = true;
+    		paramsToSend.isPartiallyOutOnX = true;
+    	}
+    	//outside Y
+    	if(target.y+target.h < 0 ||
+    		target.y > canvas.height){
+    		paramsToSend.isEntirelyOutOnY = true;
     	}
     	if(target.y < 0 ||
     		target.y+target.h > canvas.height){
-    		paramsToSend.isOutOnY = true;
+    		paramsToSend.isPartiallyOutOnY = true;
     	}
     	eventBus.emit("outside canvas response", paramsToSend);
     });
