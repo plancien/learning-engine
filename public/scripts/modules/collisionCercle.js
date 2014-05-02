709a97a5f8fbb09e8809d70801831c7ac2e7314e
Aperruchot <a.perruchot@student.isartdigital.com>
2014-05-02 16:24:26 +0200

    correction des modules

--- public/scripts/modules/collisionCercle.js
+++ public/scripts/modules/collisionCercle.js
@@ -1,10 +1,13 @@
 define(['event_bus'], function(eventBus) {
 
-    eventBus.on('collisionCercle', function(object1,object2,radiusObject1,radiusObject2) 
+    eventBus.on('collisionCircle', function() 
     {
-        if((object1.x-object2.x)*(object1.x-object2.x)+(object1.y-object2.y)*(object1.y-object2.y) <= radiusObject1*radiusObject1+radiusObject2*radiusObject2)
-            return true;
-        else
-            return false;
+        function collisionCircle (object1,object2,radiusObject1,radiusObject2)
+        {
+            if((object1.x-object2.x)*(object1.x-object2.x)+(object1.y-object2.y)*(object1.y-object2.y) <= radiusObject1*radiusObject1+radiusObject2*radiusObject2)
+                return true;
+            else
+                return false;
+        }
     }           
 });
