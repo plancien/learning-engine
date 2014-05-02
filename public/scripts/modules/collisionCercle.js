c8a05f0b20718cc6d9b6eb9231b75d3bf2b5b19e
Aperruchot <a.perruchot@student.isartdigital.com>
2014-05-02 15:14:25 +0200

    indentation correction

--- public/scripts/modules/collisionCercle.js
+++ public/scripts/modules/collisionCercle.js
@@ -1,10 +1,10 @@
 define(['event_bus'], function(eventBus) {
 
-	eventBus.on('collisionCercle', function(object1,object2,radiusObject1,radiusObject2) 
-	{
-	if(Math.abs(object1.x-object2.x)+Math.abs(object1.y-object2.y)<= radiusObject1+radiusObject2)
-		return true;
-	else
-		return false;
-	}			
+    eventBus.on('collisionCercle', function(object1,object2,radiusObject1,radiusObject2) 
+    {
+    if(Math.abs(object1.x-object2.x)+Math.abs(object1.y-object2.y)<= radiusObject1+radiusObject2)
+        return true;
+    else
+        return false;
+    }           
 });
--- public/scripts/modules/randRGBA.js
+++ public/scripts/modules/randRGBA.js
@@ -1,30 +1,30 @@
 define(['event_bus'], function(eventBus) {
 
-	eventBus.on('randRGBA', function(red,green,blue,alpha) 
-	{
-		//l'on peut customisé sont random RGBA, si l'on met false, un random sera effectué, si l'on met un chiffre, il sera utilisé
-		if (red === false || red === undefined)
-			r = (Math.random()*255);
-		else
-			r = red;
+    eventBus.on('randRGBA', function(red,green,blue,alpha) 
+    {
+        //l'on peut customisé sont random RGBA, si l'on met false, un random sera effectué, si l'on met un chiffre, il sera utilisé
+        if (red === false || red === undefined)
+            r = (Math.random()*255);
+        else
+            r = red;
 
-		if (green === false || green === undefined)
-			g = (Math.random()*255);
-		else
-			g = green;
+        if (green === false || green === undefined)
+            g = (Math.random()*255);
+        else
+            g = green;
 
-		if (blue === false || blue === undefined)
-			b = (Math.random()*255);
-		else
-			b = blue;
+        if (blue === false || blue === undefined)
+            b = (Math.random()*255);
+        else
+            b = blue;
 
-		if (alpha === false || alpha === undefined)
-			a = Math.random();
-		else
-			a = alpha;
+        if (alpha === false || alpha === undefined)
+            a = Math.random();
+        else
+            a = alpha;
 
-		rgba="rgba("+r+","+g+","+b+","+a+")";
+        rgba="rgba("+r+","+g+","+b+","+a+")";
 
-		return rgba;
-	}
+        return rgba;
+    }
 });
