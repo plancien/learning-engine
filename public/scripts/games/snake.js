b517413459d3af6bb5b9cf1dcdcafb781961b6fb
shinZ3PHYR <f.allard@student.isartdigital.com>
2014-05-02 14:37:11 +0200

    snake environement console.log("olol");

--- public/scripts/games/snake.js
+++ public/scripts/games/snake.js
@@ -1,15 +1,17 @@
-
-window.onload=function init(){
-    var game = {
-        canvas: document.getElementById("canvas"),
-        ctx: document.getElementById("canvas").getContext('2d'),
-        run:function run(){
+define([
+    'event_bus',
+    'event_capabilities'
+], function(eventBus, addEventCapabilities) {
+        var game = {};
+        game.canvas: document.getElementById("canvas"),
+        game.ctx: document.getElementById("canvas").getContext('2d'),
+        game.run = function run(){
             window.requestAnimationFrame(function(){game.run()});
             this.ctx.fillStyle ='white';
             this.ctx.fillRect(0,0,canvas.width,canvas.height)
            
         },
-        player:{
+        game.player:{
             x:0,
             y:0,
             w:64,
@@ -32,9 +34,9 @@ window.onload=function init(){
                     break;
                 } 
             },
-            // addBlock:function addBlock(){
-            //     console.log("lool");
-            // },
+            addBlock:function addBlock(){
+                console.log("lool");
+            },
         }
 
     };
@@ -81,46 +83,37 @@ function run(game){
 
 
 
-function addEventCapabilities (object) {
+    function addEventCapabilities (object) {
     
-    var listenersFor = {};  
-    object.on = function (eventName, callback) {
-       listenersFor[eventName] = listenersFor[eventName] || [];
-       listenersFor[eventName].push(callback); 
-    };
+        var listenersFor = {};  
+        object.on = function (eventName, callback) {
+           listenersFor[eventName] = listenersFor[eventName] || [];
+           listenersFor[eventName].push(callback); 
+        };
 
-    object.emit = function () {
-        var args = Array.prototype.slice.call(arguments);
-        console.log("arguments : ", arguments);
-        console.log("args :", args);
-        //shift récupère la première valeur du tableau,la return et la supprime args.pop() le dernier argument.
-        var eventName = args.shift();
-        // si event est associé listeners == event sinon pas d'event donc tableau vide
-        var listeners = listenersFor[eventName] || [];
-        // on parcourt la liste des function liée a cet event.
-        for(var i= 0; i < listeners.length; i++) {
-            try {
-                //apply appelle la fonction en lui passant en paramêtre le contenu du  tableau args.
-                //args est une liste d'arguments que l'on passe à la fonction contenue dans listeners[i]. 
-                //Le premiers argument remplace la variable "this" dans la fonction. 
+        object.emit = function () {
+            var args = Array.prototype.slice.call(arguments);
+            console.log("arguments : ", arguments);
+            console.log("args :", args);
+            //shift récupère la première valeur du tableau,la return et la supprime args.pop() le dernier argument.
+            var eventName = args.shift();
+            // si event est associé listeners == event sinon pas d'event donc tableau vide
+            var listeners = listenersFor[eventName] || [];
+            // on parcourt la liste des function liée a cet event.
+            for(var i= 0; i < listeners.length; i++) {
+                try {
+                    //apply appelle la fonction en lui passant en paramêtre le contenu du  tableau args.
+                    //args est une liste d'arguments que l'on passe à la fonction contenue dans listeners[i]. 
+                    //Le premiers argument remplace la variable "this" dans la fonction. 
 
-                //maFonction.apply(null,[1,2,3]) <=> maFonction(1,2,3);
-                listeners[i].apply(object, args);
-            } catch(e) {
-                console.log('Error on event ' + eventName);
-                // bloque le script et renvoie l'event (e) qui a provoqué l'erreur.
-                throw(e);
+                    //maFonction.apply(null,[1,2,3]) <=> maFonction(1,2,3);
+                    listeners[i].apply(object, args);
+                } catch(e) {
+                    console.log('Error on event ' + eventName);
+                    // bloque le script et renvoie l'event (e) qui a provoqué l'erreur.
+                    throw(e);
+                }
             }
-        }
-    };
-}
-/*var game = {};
-addEventCapabilities(game);
-game.on('connected', function a (name) {
-    // this va etre remplacé par le 1er argument d' apply() en ligne 28
-    console.log("Une personne s'est connectée, c'est " + name);
+        };
+    }
 });
-game.on('connected', function b () {
-    console.log("je suis heureux !" );
-});
-game.emit('connected', 'arg1', 'arg2');*/
