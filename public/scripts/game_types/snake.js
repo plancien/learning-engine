93faf55800b2bed27f8c2d63a5a0f5928c7ee9bc
shinZ3PHYR <f.allard@student.isartdigital.com>
2014-05-02 15:37:00 +0200

    canvas up

--- public/scripts/game_types/snake.js
+++ public/scripts/game_types/snake.js
@@ -1,8 +1,11 @@
 define([
     'event_bus',
     'modules/canvas',
-], function(eventBus, canvasModule) {
-
-
+], function(eventBus, Canvas) {
+	var game = {};
+	game.canvas = Canvas.create({"width" : 800, "height" : 600});
+	console.log(game.canvas.width)
+	game.canvas.context.fillStyle = "rgb(0,0,0)";
+	game.canvas.context.fillRect(0,0, 800, 600); 
 	alert("hello world");
 });
