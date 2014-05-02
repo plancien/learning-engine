cf709eab1aa77f73264882be6c7432ed16c0d7c3
Woreck <titim-du-60@hotmail.fr>
2014-05-02 17:28:11 +0200

    Formating du code timotey.js

--- public/scripts/game_types/timotey.js
+++ public/scripts/game_types/timotey.js
@@ -66,121 +66,159 @@ define([
 
               this.type = type;
               this.modification = {};
+
               for(var key in modification){
                 this.modification[key] = modification[key];
               }
+
               this.useOnPlayer = function(player){
                 // player = Player()
                 // dataUpdate = modif to player
                 // dataSend = modif to player + id power up a suppr
                 var dataUpdate = {id:player.id};
                 var dataSend = {idPlayer:player.id, idPowerUp:this.id, playerModification:{}};
+
                 for(var key in this.modification){
                   player[key] += this.modification[key];
                   dataUpdate[key] = player[key];
                   dataSend.playerModification[key] = player[key];
                 }
+
                 connector.emit("infoToSync -g",{id:player.id,eventName:"player get powerup",objectKey:"users",
                   update:dataUpdate,
                   send:dataSend
                 });
+
                 connector.emit("delete info -g",{objectKey:"powerUps",idObject:this.id});
               };
+
               this.draw = function(context){
                 context.fillStyle = this.color;
                 context.fillRect(this.x,this.y,this.w,this.h);
               };
+
               this.collision = function(object){
+
                 if((this.x >= object.x + object.w)      // trop à droite
                     || (this.x + this.w <= object.x) // trop à gauche
                     || (this.y >= object.y + object.h) // trop en bas
                     || (this.y + this.h <= object.y))  // trop en haut
-                      return false 
+                      return false;
                     else{
                       this.useOnPlayer(object);
                       return true;
                     }
-              }
+              };
+
             };
 
             //COMPONENT NPC (AKA PLAYER)
             function NPC(x, y, w, h, id, health, maxHealth, alive, color, score){
+
+               this.id = id || Math.random()*1000;
+               this.score = score || 0;
+
                this.x = x || 0;
                this.y = y || 0;
                this.w = w || 30;
                this.h = h || 30;
-               this.id = id || Math.random()*1000;
                
                this.health = health || 30;
                this.maxHealth = maxHealth;
                this.alive = alive;
+
                this.speed = 10;
                this.bulletSpeed = 10;
                this.bulletDamage = 5;
+
                this.color = color || "white";
                this.deathColor = "black";
-               this.score = score || 0;
+               
                
                this.syncPosFromServer = function(e){
-                   if(e.id = this.id){
-                       this.move(e.x,e.y);
-                   }
+
+                  if(e.id = this.id){
+                    this.move(e.x,e.y);
+                  }
+
                };
+
                this.move = function(x,y){
                    this.x = x;
                    this.y = y;
                };
+
                this.draw = function(context){
+
                 if(this.alive){
                   context.fillStyle = this.color;
                 }
                 else{
                   context.fillStyle = this.deathColor;
                 }
+
                 context.fillRect(this.x,this.y,this.w,this.h);
+
                };
+
                this.takeDamageNPC = function(amountOfDamage){
                 this.health -= amountOfDamage;
-               }
+               };
+
                this.killNPC = function(){
                 this.alive = false;
-               }
+               };
+
                this.respawn = function(){
+
                 this.alive = true;
+                this.health = this.maxHealth;
+
                 this.x = Math.random() * 700;
                 this.y = Math.random() * 500;
                 this.w = 30;
                 this.h = 30;
 
-                this.health = this.maxHealth;
                 this.speed = 10;
                 this.bulletSpeed = 10;
                 this.bulletDamage = 5;
+
                };
             };
 
             //CLASS BULLET
             function Bullet(x, y, vector, speed, id, idInArray, color, damage){
+              this.id = id;
+              this.idInArray = idInArray;
+
+              this.alive = true;
+              
               this.x = x;
               this.y = y;
               this.w = 4;
               this.h = 4;
+              
               this.vector = vector;
               this.speed = speed;
-              this.id = id;
-              this.idInArray = idInArray;
+              
               this.color = color
+              
               this.damage = damage || 10;
-              this.alive = true;
 
               this.move = function(){
+
                 var oldPosition = { x: this.x, y: this.y};
-                   this.x += this.vector.x*this.speed;
-                   this.y += this.vector.y*this.speed;
-               };
+
+                this.x += this.vector.x*this.speed;
+                this.y += this.vector.y*this.speed;
+
+              };
+
               this.draw = function(context){
+
                context.fillStyle = this.color;
                context.fillRect(this.x,this.y,this.w,this.h);
+
               };
             };
 
@@ -193,13 +231,16 @@ define([
 
             //COMPONENT INPUT CONTROL
             function addInputControl(object){
-               eventBus.on('keys still pressed', function(e){
+
+              eventBus.on('keys still pressed', function(e){
                    var oldPosition = {
                        x:object.x,
                        y:object.y
                    };
+
                    var previewX = object.x;
                    var previewY = object.y;
+                   
                    if(e == "Q"){
                       previewX = object.x - object.speed;
                    }
@@ -212,16 +253,19 @@ define([
                    if(e == "S"){
                       previewY = object.y + object.speed;
                    }
+                   
                    eventBus.emit("outside canvas",{canvas:canvas.canvas,target:{oldX:oldPosition.x,oldY:oldPosition.y,x:previewX,y:previewY,w:object.w,h:object.h}});
-               });
-                eventBus.on("outside canvas response",function(params){
-                   if(!params.isPartiallyOutOnX){
+              });
+
+              eventBus.on("outside canvas response",function(params){
+
+                  if(!params.isPartiallyOutOnX){
                      object.x = params.x;
-                   }
-                   if(!params.isPartiallyOutOnY){
+                  }
+                  if(!params.isPartiallyOutOnY){
                      object.y = params.y;
-                   }
-                   if(object.x != params.oldX || object.y != params.oldY){
+                  }
+                  if(object.x != params.oldX || object.y != params.oldY){
                     connector.emit('infoToSync -g', {id:object.id,eventName:"new position",objectKey:"users",update:{
                         x:object.x,y:object.y
                       },
@@ -229,26 +273,38 @@ define([
                         x:object.x,y:object.y
                       }
                     });
-                   }
-                });
-               eventBus.on("mouse left is clicking",function(mouse){
+                  }
+
+              });
+              
+              eventBus.on("mouse left is clicking",function(mouse){
+
                 if(object.alive){
+
                   var vector ={
                     x:mouse.canvasX-(object.x+(object.w*0.5)),
                     y:mouse.canvasY-(object.y+(object.h*0.5))
                   };
+
                   var longueur = Math.sqrt(vector.x*vector.x+vector.y*vector.y);
+
                   vector.x /= longueur;
                   vector.y /= longueur;
+
                   object.shoot(vector);
+
                 }
+
                });
             };
 
             //COMPONENT SHOOT CAPABILITIES
             function addShootCapabilities(object){
+
               object.shoot = function(vector){
+
                 bullets[this.id].push(new Bullet( this.x+this.w*0.5, this.y+this.h*0.5, vector, this.bulletSpeed, this.id, bullets[this.id].length, this.color, this.bulletDamage));
+                
                 connector.emit("infoToSync -g", {id:this.id,eventName:"player shoot",objectKey:"bullets",
                   update:{
                     x: this.x+this.w*0.5, y: this.y+this.h*0.5, speed: this.bulletSpeed, vectorSend: {x:vector.x,y:vector.y}, id: this.id, idInArray: bullets[this.id].length-1, color: this.color, damage: this.bulletDamage
@@ -257,7 +313,9 @@ define([
                     x: this.x+this.w*0.5, y: this.y+this.h*0.5, speed: this.bulletSpeed, vectorSend: {x:vector.x,y:vector.y}, id: this.id, idInArray: bullets[this.id].length-1, color: this.color, damage: this.bulletDamage
                   }
                 });
+
               };
+
             };
 
             //RETURN DOM SCORE
@@ -267,14 +325,17 @@ define([
               domScore.style.color = players[id].color;
               domScore.innerHTML = id + " : "+ score;
               document.body.appendChild(domScore);
+
               if(own){
                 var changeName = document.createElement("input");
                 changeName.id = id;
                 document.body.appendChild(changeName);
+
                 var btn = document.createElement("input");
                 btn.type = "button";
                 btn.id = id;
                 btn.value = "send";
+
                 btn.onclick = function(){
                   domScore.innerHTML = changeName.value + " : " + score;
                   players[id].name = changeName.value;
@@ -289,6 +350,7 @@ define([
                     }
                   });
                 };
+
                 document.body.appendChild(btn);
               }
             };
@@ -296,16 +358,21 @@ define([
             //UPDATE DOM ELEMENT
             function updateScoreDOM(id){
               var player = players[id];
+
               var element = document.getElementById(player.id);
               element.innerHTML = (player.name || player.id) + " : "+ player.score; 
             };
 
             //DEATH SENDING CAPABILITIES
             function addSendDeathCapabilities(object){
+
               object.respawnDelay = 180;
               object.frameSinceDeath = 0;
+
               object.takeDamage = function(amountOfDamage,idKiller){
+                
                 this.health -= amountOfDamage;
+
                 connector.emit("infoToSync -g",{id:object.id,eventName:"player hit",objectKey:"users",
                   update:{
                     id: object.id, health: object.health
@@ -314,12 +381,16 @@ define([
                     id: object.id, health: object.health
                   }
                 });
+
                 if(this.health <= 0){
                   object.kill(idKiller)
                 }
-              }
+
+              };
+
               object.kill = function(idKiller){
                 this.alive = false;
+
                 connector.emit("infoToSync -g",{id:object.id, eventName:"player kill",
                   update:{
                     id: object.id,
@@ -330,11 +401,16 @@ define([
                     idKiller:idKiller
                   }
                 });
+
                 players[idKiller].score+=100;
+
                 updateScoreDOM(idKiller);
-              }
+              };
+
               object.collision = function(targetArray){
+
                 var arrayColliding = [];
+                
                 for(var i = 0 ; i < targetArray.length; i++){
                   if((targetArray[i].x >= object.x + object.w)      // trop à droite
                     || (targetArray[i].x + targetArray[i].w <= object.x) // trop à gauche
@@ -344,13 +420,19 @@ define([
                     else
                       arrayColliding.push({bullet:targetArray[i],i:i}); 
                 }
+                
                 return arrayColliding;
-              }
+              };
+
               object.waitForSpawn = function(){
+                
                 this.frameSinceDeath++;
+                
                 if(this.frameSinceDeath >= this.respawnDelay){
+
                   this.frameSinceDeath = 0;
                   this.respawn();
+
                   connector.emit("infoToSync -g",{id:this.id,eventName:"player respawn",
                     update:{
                       x:this.x,y:this.y,w:this.w,h:this.h,bulletSpeed:this.bulletSpeed,bulletDamage:this.bulletDamage,speed:this.speed,health:this.maxHealth,id:this.id,alive:true
@@ -359,8 +441,10 @@ define([
                       x:this.x,y:this.y,w:this.w,h:this.h,bulletSpeed:this.bulletSpeed,bulletDamage:this.bulletDamage,speed:this.speed,health:this.maxHealth,id:this.id,alive:true
                     }
                   });
+
                 }
-              }
+              
+              };
             };
 
             /*********************************************
@@ -376,6 +460,7 @@ define([
 
             //LOAD PLAYERS SUBSCRIBING
             connector.on("load players",function(users){
+
               for(var key in users){
                 if(users[key].id != ownPlayerId){
                   players[users[key].id] = new NPC( users[key].x, users[key].y, users[key].w, users[key].h, users[key].id, users[key].health, users[key].maxHealth, users[key].alive, users[key].color, users[key].score);
@@ -383,13 +468,16 @@ define([
                   createScoreDOM(users[key].id,users[key].score);
                 }
               }
+
             });
 
             //LOAD POWER UP SUBSCRIBING
             connector.on("load powerUps",function(powerUps){
+
               for(var key in powerUps){
                 powerUp[powerUps[key].id] = new PowerUp( powerUps[key].x, powerUps[key].y, powerUps[key].w, powerUps[key].h, powerUps[key].color, powerUps[key].id, powerUps[key].type, powerUps[key].modification);
               }
+              
             });
 
             //EMIT OWN PLAYER CREATED
@@ -412,15 +500,19 @@ define([
 
             //NAME CHANGED SUBSCRIBING
             connector.on("name changed", function(data){
+
               players[data.id].name = data.name;
               updateScoreDOM(data.id);
+
             });
 
             //NEW PLAYER SUBSCRIBING
             connector.on("new player", function(player){
+
                players[player.id] = new NPC(player.x, player.y, player.w, player.h, player.id, player.health, player.maxHealth, player.alive, player.color, player.score);
                bullets[player.id] = [];
                createScoreDOM(player.id,player.score);
+
             });
             
             //NEW POSITION SUBSCRIBING
@@ -430,17 +522,22 @@ define([
 
             //PLAYER DISCONNECT SUBSCRIBING
             connector.on('player disconnected',function(user){
+
                var element = document.getElementById(user.id);
                document.body.removeChild(element);
                delete players[user.id];
+
             });
             
             //PLAYER SHOOT SUBSCRIBING
             connector.on("player shoot", function(shoot){
+
               if(!bullets[shoot.id]){
                 bullets[shoot.id] = [];
               }
+
               bullets[shoot.id].push(new Bullet(shoot.x,shoot.y,shoot.vectorSend,shoot.speed,shoot.id, shoot.idInArray,shoot.color));
+
             });
 
             //PLAYER RESPAWN SUBSCRIBING
