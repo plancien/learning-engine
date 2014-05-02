5537a04c6f81334fa8e723ce9503acb4190db2ee
Marv-jo <m.polette@student.isartdigital.com>
2014-05-02 14:26:03 +0200

    rush the princess

---
+++ public/scripts/game_types/rushThePrincess.js
@@ -0,0 +1,24 @@
+define([
+    'event_bus',
+    'modules/canvas',
+], function(eventBus, canvasModule) {
+	/*function Sprite() {
+		this.image = new Image();
+		this.image.src = src;
+		this.x = x || 10;
+		this.y = y || 10;
+		this.left = false;
+		this.right = false;
+		this.speed = 5;
+		this.frameWidth = 0;
+		this.frameHeight = 0;
+		this.animFrame = 0;
+	}
+
+	Sprite.prototype.drawSprite = function(ctx) {
+		this.animFrame ++;
+		ctx.drawImage(this.image,  this.frameWidth, this.frameHeight, 50, 60, this.x, this.y, 50, 60);
+	};*/
+
+	alert("hello world");
+}
