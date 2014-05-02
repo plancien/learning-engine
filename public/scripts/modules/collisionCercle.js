fd91db726b157c78ab6713e1281b53bf0c3ac523
Aperruchot <a.perruchot@student.isartdigital.com>
2014-05-02 15:32:41 +0200

    Optimasation et indentation
    correction de l'indentation des module de collision, et optimisation du
    module de collision cercle ( converstion de Math.abs, prenant plus de
    capacité que de multiplié les variable par elle même)

--- public/scripts/modules/collisionCercle.js
+++ public/scripts/modules/collisionCercle.js
@@ -2,9 +2,9 @@ define(['event_bus'], function(eventBus) {
 
     eventBus.on('collisionCercle', function(object1,object2,radiusObject1,radiusObject2) 
     {
-    if(Math.abs(object1.x-object2.x)+Math.abs(object1.y-object2.y)<= radiusObject1+radiusObject2)
-        return true;
-    else
-        return false;
+        if((object1.x-object2.x)*(object1.x-object2.x)+(object1.y-object2.y)*(object1.y-object2.y) <= radiusObject1*radiusObject1+radiusObject2*radiusObject2)
+            return true;
+        else
+            return false;
     }           
 });
--- public/scripts/modules/collisionSquare.js
+++ public/scripts/modules/collisionSquare.js
@@ -2,15 +2,15 @@ define(['event_bus'], function(eventBus) {
 
     eventBus.on('collisionSquare', function(object1,object2) 
     {
-    if
-    (
-        (object1.x < (object2.x + object2.width)) && 
-        ((object1.x + object1.width) > object2.x) && 
-        (object1.y < (object2.y + object2.height)) && 
-        ((object1.y + object1.height) > object2.y)
-    )
-        return true;
-    else
-        return false;
+        if
+        (
+            (object1.x < (object2.x + object2.width)) && 
+            ((object1.x + object1.width) > object2.x) && 
+            (object1.y < (object2.y + object2.height)) && 
+            ((object1.y + object1.height) > object2.y)
+        )
+            return true;
+        else
+            return false;
     }           
 });
