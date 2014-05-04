f4bebf849463451833e6f36fc9a966be21ad6941
gamay777 <m.gammaitoni@student.isartdigital.com>
2014-05-04 22:11:59 +0200

    snake ReReBorn

--- public/scripts/game_types/snake.js
+++ public/scripts/game_types/snake.js
@@ -4,58 +4,118 @@ define([
     'modules/frames',
     'event_capabilities',
 ], function(eventBus, Canvas, Frames, addEventCapabilities) {
-	var game = {};
-	
+    var game = {};
+    
     var canvas = Canvas.create({"width" : 800, "height" : 600});
-
-	game.canvas = canvas.canvas;
-	game.context = canvas.context;
-	console.log(game.canvas)
-	game.context.fillStyle = "rgb(0,0,0)";
-	game.context.fillRect(0,0, 800, 600); 
-	alert("hello world");
-	game.snake = new Snake(100, 100, game);
-    addEventCapabilities(game.snake);
-    game.snake.on('move',function(e){
-        console.log("loooool",this)
-        this.move(e);
-    });
+    game.tileSize =32;
+    game.canvas = canvas.canvas;
+    game.context = canvas.context;
+    console.log(game.canvas)
+    game.context.fillStyle = "rgb(0,0,0)";
+    game.context.fillRect(0,0, 800, 600); 
+    game.snake = new Snake(100, 100, game);
     addEventCapabilities(game);
-	game.snake.display();
-	console.log(game.snake)
-    eventBus.on("new frame",function(){console.log("lol")})
-	// grille(game);
+    game.on('move snake', function(e){
+        game.snake.move(e); // C'est ici le PB  ? yes !
+    });
+    window.onkeydown = function(e){
+        console.log(e);
+        switch(e.keyCode){
+            case 38 : 
+            game.emit('move snake',"up");
+            break;
+            case 40 : 
+            game.emit('move snake',"down");
+            break;
+            case 39 :
+            game.emit('move snake','right');
+            break;
+            case 37 : 
+            game.emit('move snake',"left");
+            break;
+        }
+    };
+    console.log(game.snake)
+    eventBus.on("new frame",function(){run(game)})
+    // grille(game);
 });
 function Snake(x,y,game){
-	this.refGame = game;
-	this.x = x;
-	this.y = y;
-	this.width = 32;
-	this.height = 32;
-	this.size = 4;
-    move:function move(args){
-                console.log(this);
-                switch(args){
-                    case 'up':
-                    this.y--;
-                    break;
-                    case 'down':
-                    this.y++;
-                    break;
-                    case 'gauche':
-                    this.x-=40;
-                    break;
-                    case 'droite':
-                    this.x++;
-                    break;
-                } 
+    this.refGame = game;
+    this.pos={
+        x:0,
+        y:0,
+    }
+    this.moveDelay= 30;
+    this.x = this.pos.x*game.tileSize;
+    this.y = this.pos.y*game.tileSize;
+    this.width = game.tileSize;
+    this.height = game.tileSize;
+    this.size = 4;
+    this.input="left";
+    this.move = function move(args){
+        switch(args){
+            case 'up':
+            this.input="up";
+            break;
+            case 'down':
+            this.input="down";
+            break;
+            case 'left':
+            this.input="left";
+            break;
+            case 'right':
+            this.input="right";
+            break;
+        } 
+    };
+    this.update=function update(){
+        this.moveDelay--;
+        this.x = this.pos.x*game.tileSize;
+        this.y = this.pos.y*game.tileSize;
+        switch (this.input){
+            case "right":
+            if(this.moveDelay==0){
+                this.pos.x ++;
+                this.moveDelay=30;   
             }
+            break;
+            case "left":
+            if(this.moveDelay==0){
+                this.pos.x --;
+                this.moveDelay=30; 
+            }
+            break;
+            case "up":
+            if(this.moveDelay==0){
+                this.pos.y --;
+                this.moveDelay=30;
+            }
+            break;
+            case "down":
+            if(this.moveDelay==0){
+                this.pos.y ++;
+                this.moveDelay=30;
+            }
+            break;
+        }
+    }
 };
+
+function run(game){
+    game.context.fillStyle="black";
+    game.context.fillRect(0,0,game.canvas.width,game.canvas.height);
+    console.log(game.snake.input);
+    game.snake.update();
+    draw(game);
+}
+function draw(game){
+  game.snake.display();  
+}
     
 Snake.prototype.display = function() {
-	this.refGame.context.fillStyle = "white";
-	this.refGame.context.fillRect(this.x, this.y, this.x+this.width, this.y+this.height);
-	
+    this.refGame.context.fillStyle = "red";
+    this.refGame.context.fillRect(this.x, this.y, this.width, this.height);
+    
 }
 function grille(game){
 	var carre = {}
