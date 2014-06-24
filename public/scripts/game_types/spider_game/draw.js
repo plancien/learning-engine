6a37e33a4b4a5742b2785a7b015f35f82c9f7001
0Simba <b.dersoir@student.isartdigital.com>
2014-06-24 13:05:29 +0200

    Add circle image on spiderGame

--- public/scripts/game_types/spider_game/draw.js
+++ public/scripts/game_types/spider_game/draw.js
@@ -77,9 +77,16 @@ define(["game_types/spider_game/config"], function(config){
     function drawAnchor(ctx,scrolling,imgs,player,anchors) {
         for (var i = 0; i < anchors.length; i++) {
             ctx.drawImage(anchors[i].img,
-                          anchors[i].x-anchors[i].radius,
-                          anchors[i].y-anchors[i].radius-scrolling
+                              anchors[i].x-anchors[i].radius,
+                              anchors[i].y-anchors[i].radius-scrolling
             );
+
+            ctx.beginPath();
+            // ctx.arc(anchors[i].x, anchors[i].y, anchors[i].radius, 0, 2 * Math.PI, false);
+            ctx.arc(anchors[i].x, anchors[i].y-scrolling, anchors[i].radius, 0, 2 * Math.PI, false);
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 2;
+            ctx.stroke();
         };
     }
 
