bb66a6ef40d2834d50a9492fb25f6a25834c94ff
0Simba <b.dersoir@student.isartdigital.com>
2014-07-04 10:52:39 +0200

    Can use user images

--- server/url_model/create_game.js
+++ server/url_model/create_game.js
@@ -7,54 +7,53 @@ var users = require(__path.model + "/users");
 
 
 module.exports = function(){
-	pageManager.add("create_game", 
-	function(req, res){				//get
-		var that = this;
-		if (userName = req.session.userName){
+    pageManager.add("create_game", 
+    function(req, res){             //get
+        var that = this;
+        if (userName = req.session.userName){
 
-			var spec = {}
-			spec.gameList = games.getGamesList();
-
-	        images.getDefaultUrl(function(err,imgs) {
-	            spec.imagesList = imgs;
-	            spec.gameList = games.getGamesList()
-				that.display(req, res, "create_game", spec);
-	        });
-	    }
-	    else{
-	    	res.redirect("/login");
-	    }
-	},
-	function(req, res){				//post
-		if (userName = req.session.userName){
-			checkPostValue(req.body, function(err){
-				if (err.length > 0) res.send(err)
-				else{
-			        games.addGame(req.body, function(gameData){
-			        	users.addGame(userName, gameData.name, function(){
-			        		res.redirect("/game/"+gameData.name);
-			        	});
-			        });
-				}	
-			})
-		}
-		else{
-	    	res.redirect("/login");
-		}
-	}
+            var spec = {};
+            images.getDefaultUrl(function(err,imgs) {
+                spec.imagesList = imgs;
+                spec.userImagesList = users.getUserImageSync(userName);
+                spec.gameList = games.getGamesList();
+                that.display(req, res, "create_game", spec);
+            });
+        }
+        else{
+            res.redirect("/login");
+        }
+    },
+    function(req, res){             //post
+        if (userName = req.session.userName){
+            checkPostValue(req.body, function(err){
+                if (err.length > 0) res.send(err);
+                else{
+                    games.addGame(req.body, function(gameData){
+                        users.addGame(userName, gameData.name, function(){
+                            res.redirect("/game/"+gameData.name);
+                        });
+                    });
+                }
+            })
+        }
+        else{
+            res.redirect("/login");
+        }
+    }
 )};
 
 
 function checkPostValue(post, callback){
-	var error = [];
-	if (!post.question)
-		error.push("Vous n'avez pas entré de question");
-	if (!post.bonus)
-		error.push("Vous n'avez pas selectionné d'image bonus");
-	if (!post.malus)
-		error.push("Vous n'avez pas selectionné d'image malus");
-	if (!post.game)
-		error.push("Vous n'avez pas selectionné de jeu");
-	callback(error);
+    var error = [];
+    if (!post.question)
+        error.push("Vous n'avez pas entré de question");
+    if (!post.bonus)
+        error.push("Vous n'avez pas selectionné d'image bonus");
+    if (!post.malus)
+        error.push("Vous n'avez pas selectionné d'image malus");
+    if (!post.game)
+        error.push("Vous n'avez pas selectionné de jeu");
+    callback(error);
 };
 
